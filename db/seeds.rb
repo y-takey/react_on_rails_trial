d8c8b1090559f95dab1b73b23db4e364adfcb7ed
takeyuichi <y.takey@gmail.com>
2017-11-26 16:22:10 +0900

    Create test users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+["user1", "user2", "user3"].each do |user|
+  User.create!(email: user, password: user)
+end
