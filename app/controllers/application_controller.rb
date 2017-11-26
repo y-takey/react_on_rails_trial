21fd9e80f7e652516698587af79ed79877192ea6
takeyuichi <y.takey@gmail.com>
2017-11-26 16:31:09 +0900

    Add to authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, prepend: true
+  before_action :authenticate_user!, except: :root
 
   def root
   end
