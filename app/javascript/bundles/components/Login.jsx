95ce8cb373b7c66ecdefd50b54639026ce94e068
Yuichiro Take <y.takey@gmail.com>
2017-11-28 14:36:33 +0900

    implement authentication logic

--- app/javascript/bundles/components/Login.jsx
+++ app/javascript/bundles/components/Login.jsx
@@ -1,21 +1,46 @@
 import PropTypes from "prop-types";
 import React from "react";
+import { FormGroup } from "material-ui/Form";
 import TextField from "material-ui/TextField";
 import Button from "material-ui/Button";
+import axios from "axios";
+
+// const csrfToken = document.querySelector(`meta[name="csrf-token"]`).content;
+//
+// axios.defaults.headers.common["X−CSRF-Token"] = csrfToken;
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 export default class Login extends React.Component {
   state = {
+    email: "",
+    password: "",
     redirectToReferrer: false
   };
 
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+
   login = () => {
-    // TODO: request to login then
-    // this.setState({ redirectToReferrer: true });
+    const { email, password } = this.state;
+    const [csrfParam, csrfToken] = ["csrf-param", "csrf-token"].map(
+      key => document.querySelector(`meta[name="${key}"]`).content
+    );
+    axios
+      .post("/users/sign_in", { user: { email, password }, [csrfParam]: csrfToken })
+      .then(response => {
+        console.log("auth success.");
+      })
+      .catch(error => {
+        console.error("auth failed.");
+      });
   };
 
   render() {
     const { from } = this.props.location.state || { from: { pathname: "/" } };
-    const { redirectToReferrer } = this.state;
+    const { email, password, redirectToReferrer } = this.state;
 
     if (redirectToReferrer) {
       return <Redirect to={from} />;
@@ -23,11 +48,32 @@ export default class Login extends React.Component {
 
     return (
       <form noValidate autoComplete="off">
-        <TextField id="name" label="Name" margin="normal" />
-        <TextField id="password" label="Password" type="password" autoComplete="current-password" margin="normal" />
-        <Button raised color="primary">
-          LOGIN
-        </Button>
+        <FormGroup row>
+          <TextField
+            id="email"
+            label="Name"
+            type="email"
+            margin="normal"
+            value={email}
+            onChange={this.handleChange("email")}
+          />
+        </FormGroup>
+        <FormGroup row>
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            margin="normal"
+            value={password}
+            onChange={this.handleChange("password")}
+          />
+        </FormGroup>
+        <FormGroup row>
+          <Button raised color="primary" onClick={this.login}>
+            LOGIN
+          </Button>
+        </FormGroup>
       </form>
     );
   }
